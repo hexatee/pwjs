640f73ce129bac976b32438c34e0bf798fa3eb75
hexatee <80221860+hexatee@users.noreply.github.com>
2021-06-06 22:48:21 +0200

    lab 13

--- lab12/py_script.py
+++ lab12/py_script.py
@@ -1,19 +1,74 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin,login_required, login_user, logout_user
 import sqlite3
 
 
 app=Flask(__name__)
 
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='sekretny_klucz'
+)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+    def __init__(self,id):
+        self.id = id
+        self.name = "user"+str(id)
+        self.password = self.name+"_secret"
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+users = [User(id) for id in range(1,10)]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == username + "_secret":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            return abort(401)
+    else:
+        return render_template('login.html', tytul='Zaloguj się')
+
+@app.errorhandler(401)
+def page_not_found(e):
+    tytul="Coś poszło nie tak..."
+    blad = "401"
+    return render_template('blad.html', tytul=tytul, blad=blad)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template('logout.html', tytul='Wylogowanie')
+
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
+
 @app.route('/dodaj')
+@login_required
 def dodaj():
     return render_template('dodaj.html', tytul="Dodaj pracownika")
 
 @app.route('/dodajrekord', methods=['POST','GET'])
+@login_required
 def dodajrekord():
     try:
         imienazwisko = request.form['imienazwisko']
@@ -37,6 +92,7 @@ def dodajrekord():
 
 
 @app.route('/lista')
+@login_required
 def lista():
     conn = sqlite3.connect('pracownicy.db')
     conn.row_factory = sqlite3.Row
